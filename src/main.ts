a46c7152a88c88a3bededa35e83da64698f90bd5
BopAdam <mehesadam.777@gmail.com>
2026-06-30 15:15:47 +0200

    masodik feltoltes

--- src/main.ts
+++ src/main.ts
@@ -222,3 +222,76 @@ interface Config {
   readonly apiKey:string;
   dbName: string;
 }
+const myConfig: Config = {apiKey: "Kiskutya1234", dbName: "prod_db"};
+myConfig.dbName = "test_db"; // Ez rendben van
+// myConfig.apiKey = "ÚjKulcs"; // Hiba! A TS jelzi, hogy a readonly tulajdonság nem módosítható.
+
+
+//15. Alapértelmezett Osztály (Class) TypeScriptben
+/*
+Osztályok gépelése és konstruktorok működése.
+Feladat: Hozz létre egy Car osztályt brand és hp (lóerő) tulajdonságokkal, 
+és egy metódussal, ami kiírja az autó adatait
+ */
+
+ class Auto {
+  marka: string;
+  Hp: number;
+  sofor: string;
+
+  constructor(marka: string, Hp: number, Elon_Musk: string) {
+    this.marka = marka;
+    this.Hp = Hp;
+    this.sofor = Elon_Musk;
+  }
+  display() {
+    console.log(`Ez egy ${this.marka} autó, ami ${this.Hp} lóerős. A sofőr: ${this.sofor}`);
+  }
+ }
+
+ const azEnAutom = new Auto("Tesla", 999, "Elon Musk");
+azEnAutom.display();
+
+
+//16. Access Modifiers (Láthatósági módosítók)
+/*Koncepció: A public, private és protected kulcsszavak az osztályok 
+adatainak védelmére.
+Feladat: Módosítsd az osztályt úgy, hogy a rejtettKód változó private legyen, 
+azaz az osztályon kívülről ne lehessen közvetlenül átírni. 
+*/
+class Safe {
+  public name: string;
+  private SecretCode: number;
+
+constructor(name: string, code: number) {
+  this.name = name;
+  this.SecretCode = code;
+  }
+
+}
+const vault = new Safe("Mória Bányái", 4444);
+console.log(vault.name); //--- ez igy joo
+// console.log(vault.secretCode); -> Hiba! A 'secretCode' privát tulajdonság.
+
+
+ //17.Interfész öröklődés
+ /*
+ Meglévő interfészek kibővítése új tulajdonságokkal anélkül, 
+ hogy újra le kellene írnunk őket.
+Feladat: Készíts egy Person (név, kor) interfészt, majd egy Driver interfészt, 
+ami örököl a Person-ből, de kiegészül egy licenseType tulajdonsággal.
+ */
+
+interface Person {
+  nev: string;
+  kor: number;
+  hobbi: string;
+}
+
+interface Sofőr extends Person {
+  nev: "Matthew Broderick";
+  kor: 22;
+  hobbi: "Színészet";
+  JogositvanyTipus: "B"
+};
+console.log("Sofőr adatai:", {nev: "Matthew Broderick", kor: 22, hobbi: "Színészet", JogositvanyTipus: "B"});
